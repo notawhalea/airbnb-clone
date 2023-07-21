326da985671d188184f635ad26b1fbfa4941ec29
Nikita <nickpelmog2003@gmail.com>
2023-07-22 00:54:47 +0300

    feat: add db connection

--- api/index.js
+++ api/index.js
@@ -1,8 +1,35 @@
 const express = require("express");
+const cors = require("cors");
+const mongoose = require("mongoose");
+const UserModel = require("./models/User");
+require("dotenv").config();
 const app = express();
+const bcrypt = require("bcryptjs");
 
+const bcryptSalt = bcrypt.genSaltSync(10);
+
+app.use(express.json());
+app.use(
+  cors({
+    credentials: true,
+    origin: "http://127.0.0.1:5173",
+  }),
+);
+//Oxh7tRL2UYqRkgJ0
+mongoose.connect(process.env.MONGO_URL);
 app.get("/test", (req, res) => {
   res.json("test ok");
+});
+
+app.post("/register", async (req, res) => {
+  const { name, email, password } = req.body;
+  const userDoc = await UserModel.create({
+    name,
+    email,
+    password: bcrypt.hashSync(password, bcryptSalt),
+  });
+
+  res.json(userDoc);
 });
 
 app.listen(4000);
