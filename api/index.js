fbb476d175f417fbf6bfb459f9368856507d05fa
Nikita <nickpelmog2003@gmail.com>
2023-07-22 20:46:18 +0300

    feat: add cookies

--- api/index.js
+++ api/index.js
@@ -2,19 +2,21 @@ const express = require("express");
 const cors = require("cors");
 const mongoose = require("mongoose");
 const UserModel = require("./models/User");
+const jwt = require("jsonwebtoken");
 require("dotenv").config();
 const app = express();
 const bcrypt = require("bcryptjs");
 
 const bcryptSalt = bcrypt.genSaltSync(10);
+const jwtSecret = "dsvjds54d4dsvdsdsv54ds";
+const corsOptions = {
+  origin: "http://127.0.0.1:5173",
+  credentials: true,
+  exposedHeaders: ["set-cookie"],
+};
 
 app.use(express.json());
-app.use(
-  cors({
-    credentials: true,
-    origin: "http://127.0.0.1:5173",
-  }),
-);
+app.use(cors(corsOptions));
 //Oxh7tRL2UYqRkgJ0
 mongoose.connect(process.env.MONGO_URL);
 app.get("/test", (req, res) => {
@@ -23,13 +25,45 @@ app.get("/test", (req, res) => {
 
 app.post("/register", async (req, res) => {
   const { name, email, password } = req.body;
-  const userDoc = await UserModel.create({
-    name,
-    email,
-    password: bcrypt.hashSync(password, bcryptSalt),
-  });
+  try {
+    const userDoc = await UserModel.create({
+      name,
+      email,
+      password: bcrypt.hashSync(password, bcryptSalt),
+    });
 
-  res.json(userDoc);
+    res.json(userDoc);
+  } catch (e) {
+    res.status(422).json(e);
+  }
+});
+
+app.post("/login", async (req, res) => {
+  const { email, password } = req.body;
+  const userDoc = await UserModel.findOne({ email });
+  if (userDoc) {
+    const passOk = bcrypt.compareSync(password, userDoc.password);
+    if (passOk) {
+      jwt.sign(
+        { email: userDoc.email, id: userDoc._id },
+        jwtSecret,
+        {},
+        (err, token) => {
+          if (err) throw err;
+          res
+            .cookie("token", token, {
+              maxAge: 1000 * 60 * 60 * 24 * 7,
+              httpOnly: true,
+            })
+            .json("pass ok");
+        },
+      );
+    } else {
+      res.status(422).json("didnt match");
+    }
+  } else {
+    res.json("not found");
+  }
 });
 
 app.listen(4000);
--- client/src/pages/RegisterPage.jsx
+++ client/src/pages/RegisterPage.jsx
@@ -6,13 +6,18 @@ const RegisterPage = () => {
   const [name, setName] = useState("");
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
-  function registerUser(ev) {
+  async function registerUser(ev) {
     ev.preventDefault();
-    axios.post("/register", {
-      name,
-      email,
-      password,
-    });
+    try {
+      await axios.post("/register", {
+        name,
+        email,
+        password,
+      });
+      alert("Registration successful. Now you can log in");
+    } catch (e) {
+      alert("Registration failed");
+    }
   }
   return (
     <div className="mt-4 grow flex items-center justify-around">
@@ -37,7 +42,7 @@ const RegisterPage = () => {
             value={password}
             onChange={(ev) => setPassword(ev.target.value)}
           />
-          <button className="primary">Register</button>
+          <button className="primary mt-1">Register</button>
         </form>
         <div className="text-center py-2 text-gray-500">
           Already a member?{" "}
