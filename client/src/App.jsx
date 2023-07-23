be0e9c607fb2d6e591dab2521d73f599f66972c3
Nikita <nickpelmog2003@gmail.com>
2023-07-24 01:19:52 +0300

    feat: add personal account

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -6,6 +6,7 @@ import Layout from "./Layout.jsx";
 import RegisterPage from "./pages/RegisterPage.jsx";
 import axios from "axios";
 import { UserContextProvider } from "./UserContext.jsx";
+import AccountPage from "./pages/AccountPage.jsx";
 
 axios.defaults.baseURL = "http://127.0.0.1:4000";
 axios.defaults.withCredentials = true;
@@ -17,6 +18,7 @@ function App() {
           <Route index element={<IndexPage />} />
           <Route path="/login" element={<LoginPage />} />
           <Route path="/register" element={<RegisterPage />} />
+          <Route path="/account/:subpage?" element={<AccountPage />} />
         </Route>
       </Routes>
     </UserContextProvider>
--- client/src/Header.jsx
+++ client/src/Header.jsx
@@ -47,7 +47,7 @@ const Header = () => {
         </button>
       </div>
       <Link
-        to={"/login"}
+        to={user ? "/account" : "/login"}
         className="flex items-center gap-2 border border-gray-300 rounded-full py-2 px-4"
       >
         <svg
